6e07e559fa60836de074442d4561161ff5c0e41a
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-06-25 05:42:49 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,9 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
+╭─「 Donasi • Pulsa • Saweria」
 │ •  https://saweria.co/MrclNotWibu 
-│ •  [0813-8168-5954]
+│ •  [0813-8168-5954] telkom
+│ •  [0857-9723-4413] indosat
+│ •  [0813-8168-5954] Gopay,dana,ovo
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
